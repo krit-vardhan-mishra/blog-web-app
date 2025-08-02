623d3147c924336f008ffb6c5e19662bd5b3ccb6
krit-vardhan-mishra <aboywhoisnerdy@gmail.com>
2025-08-03 00:21:14 +0530

    Button

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { useState, useRef, useEffect, useCallback } from 'react';
 import { motion } from 'framer-motion';
 import { NotebookPen } from 'lucide-react';
-import { Button } from '../components/ui/button';
+import { Button } from '../components/ui/Button';
 
 const LandingPage = () => {
   useEffect(() => {
