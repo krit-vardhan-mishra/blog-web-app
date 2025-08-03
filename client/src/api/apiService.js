57ac966f68178d3c1d592069c90f124bae11c78c
krit-vardhan-mishra <aboywhoisnerdy@gmail.com>
2025-08-03 22:44:40 +0530

    fixed apiService.js

--- client/src/api/apiService.js
+++ client/src/api/apiService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const MODE = import.meta.env.VITE_NODE_ENV || 'PRODUCTION';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || (MODE === 'PRODUCTION' ? '/api' : 'http://localhost:5000/api');
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
